42df0448aaafab14ae2f1c73db9244a9c4f57fea
ashikur37 <piash3700@gmail.com>
2023-01-22 18:33:02 +0600

    cloudinary image added

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,19 @@ import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare()
   // integrations: [prefetch()]
   ,
-  integrations: [react()]
+
+  integrations: [react(), tailwind(
+  //   {
+  //   // Example: Provide a custom path to a Tailwind config file
+  //   config: { path: './custom-config.cjs' },
+  // }
+  )]
 });
