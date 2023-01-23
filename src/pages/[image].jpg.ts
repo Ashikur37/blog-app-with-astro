a9dc549fafc9eee30d3ded9f913d90dcc718afb1
ashikur37 <piash3700@gmail.com>
2023-01-23 19:00:45 +0600

    updated

--- src/pages/[image].jpg.ts
+++ src/pages/[image].jpg.ts
@@ -1,7 +1,11 @@
 export async function get({ params, request }:any) {
   const res=await fetch('https://res.cloudinary.com/dxlmkfscm/image/upload/v1674393390/images/'+params.image+".jpg");
   const blob= await res.blob();
-    return new Response(blob, { status: 200, });
+    return new Response(blob,
+      
+      { status: 200,headers:{
+      'cache-control':'public, max-age=86400, stale-while-revalidate=604800'
+    } });
 }
 //https://res.cloudinary.com/dxlmkfscm/image/upload/v1674393390/images/mjplseelncodaqrhoqar.jpg
-// 
+// https://res.cloudinary.com/dxlmkfscm/image/upload/v1674393390/images/mjplseelncodaqrhoqar.jpg
