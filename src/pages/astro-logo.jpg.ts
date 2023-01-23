a043a4a965254ed3178719d23f249f1fc8a4cf07
ashikur37 <piash3700@gmail.com>
2023-01-23 12:04:41 +0600

    updated

---
+++ src/pages/astro-logo.jpg.ts
@@ -0,0 +1,29 @@
+export async function get({ params, request }:any) {
+  const res=await fetch('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg');
+  const blob= await res.blob();
+      return {
+      body: blob,
+      encoding: 'binary',
+    };
+//   fetch('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg')
+//   .then(res => res.blob()) // Gets the response and returns it as a blob
+//   .then(blob => {
+//     // Here's where you get access to the blob
+//     // And you can use it for whatever you want
+//     // Like calling ref().put(blob)
+
+//     // Here, I use it to make an image appear on the page
+//     // let objectURL = URL.createObjectURL(blob);
+//     // let myImage = new Image();
+//     // myImage.src = objectURL;
+//     // document.getElementById('myImg').appendChild(myImage)
+//     return {
+//       body: "blob",
+//       // encoding: 'binary',
+//     };
+// });
+
+
+
+
+}
--- src/pages/astro-logo.png.ts
+++
@@ -1,8 +0,0 @@
-export async function get({ params, request }:any) {
-  const response = await fetch("https://astro.build/assets/press/full-logo-light.png");
-  const buffer = Buffer.from(await response.arrayBuffer());
-  return {
-    body: buffer,
-    encoding: 'binary',
-  };
-}
