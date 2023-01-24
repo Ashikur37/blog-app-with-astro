50943907b94d09a0377ea59d939571544e4c9a2f
ashikur37 <piash3700@gmail.com>
2023-01-24 21:44:35 +0600

    updated

--- src/pages/static/[width]/[height]/[...image].jpg.ts
+++ src/pages/static/[width]/[height]/[...image].jpg.ts
@@ -1,11 +1,23 @@
+function x() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('done!');
+    },7000);
+  });
+}
+
 export async function get({ params, request }:any) {
     const base="https://res.cloudinary.com/dxlmkfscm/image/";
     const res=await fetch(base+"/fetch/f_auto/q_auto/c_scale,h_"+params.height+",w_"+params.width+"/"+base+params.image+".jpg");
     const blob= await res.blob();
+    const result = await x();
+    // setTimeout(function(){
       return new Response(blob, { status: 200,headers:{
         "Cache-Control": "public, max-age=86400 stale-while-revalidate=604800",
         "content-type": "image/jpeg"
       }  });
+    // },5000)
+     
       //  return new Response(`${base}/fetch/f_auto/q_auto/c_scale,h_${params.height},w_${params.width}/${base}${params.image}.jpg`, { status: 200, });
 
       // return new
